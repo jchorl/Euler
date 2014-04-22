b46520172ed2031d8fde21ac7862aecbb83b0143
jachorlt <jachorlt@student.cs.uwaterloo.ca>
2014-04-21 22:19:36 -0400

    added p39, incomplete

---
+++ p39.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <cmath>
+using namespace std;
+
+int main(int argc, const char* argv[]){
+	for(int i= 0; i<1000; i++){
+		for(int a= 0; a<i; a++){
+			for(int b= 0; b<i; b++){
+				for(int c= 0; c<i; c++){
+					if(a+b+sqrt(pow(a,2)+pow(b,2))==(i+0.0)){
+//implement this
+					}
+				}
+			}
+		}
+	}
+}
+
