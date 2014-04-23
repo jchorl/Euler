6cc1c2a41d9ad3661bae8bc109e5c64550742ef0
jachorlt <jachorlt@student.cs.uwaterloo.ca>
2014-04-23 16:30:28 -0400

    fixed p39

--- p39.cc
+++ p39.cc
@@ -2,19 +2,29 @@
 #include <string>
 #include <cassert>
 #include <cmath>
+#include <set>
 using namespace std;
 
+double p(double a, double b){
+	return a+b+sqrt(pow(a,2)+pow(b,2));
+}
 int main(int argc, const char* argv[]){
-	for(int i= 0; i<1000; i++){
-		for(int a= 0; a<i; a++){
-			for(int b= 0; b<i; b++){
-				for(int c= 0; c<i; c++){
-					if(a+b+sqrt(pow(a,2)+pow(b,2))==(i+0.0)){
-//implement this
-					}
+	double maxSize= 0;
+	double maxP= 0;
+	for(double perimeter= 1.0; perimeter<1000; perimeter++){
+		set<double> sides;
+		for(double a= 1.0; a<perimeter; a++){
+			for(double b= 1.0; b<perimeter; b++){
+				if(sides.find(a)==sides.end()&&sides.find(b)==sides.end()&&p(a,b)==perimeter){
+					sides.insert(a);
 				}
 			}
 		}
+		if(sides.size()>maxSize){
+			maxSize= sides.size();
+			maxP= perimeter;
+		}
 	}
+	cout << "Perimeter: " << maxP << ", Solutions:" << maxSize << endl;
 }
 
