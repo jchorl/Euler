ef583640e15c70f41f0e8ba0711374457c8f4b4c
josh <jchorlton@gmail.com>
2015-03-04 08:20:38 -0800

    major speed optimization

--- p62.py
+++ p62.py
@@ -3,19 +3,13 @@ import itertools
 
 permutations = {}
 
-def finished():
-	for i in permutations:
-		if len(permutations[i]) == 5:
-			print(min(permutations[i]))
-			return True
-	return False
-
 for n in itertools.count():
-	l = list(str(n ** 3))
+	l = str(n ** 3)
 	s = "".join(sorted(l))
 	if s in permutations:
 		permutations[s].append(n ** 3)
+		if len(permutations[s]) == 5:
+			print(min(permutations[s]))
+			exit()
 	else:
 		permutations[s] = [n ** 3]
-	if finished():
-		exit()
